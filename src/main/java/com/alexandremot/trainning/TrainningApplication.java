aab56cdf1ac27a22e1a742b22d83da9ed86d278c
Alexandre Mota <alexandremot@gmail.com>
2023-08-19 23:00:27 -0300

    incluido log na controller

--- src/main/java/com/alexandremot/trainning/TrainningApplication.java
+++ src/main/java/com/alexandremot/trainning/TrainningApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.alexandremot.trainning")
+@ComponentScan
 public class TrainningApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/alexandremot/trainning/controller/AnimeController.java
+++ src/main/java/com/alexandremot/trainning/controller/AnimeController.java
@@ -1,16 +1,32 @@
 package com.alexandremot.trainning.controller;
 
 import com.alexandremot.trainning.domain.Anime;
+import com.alexandremot.trainning.util.DateUtil;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.time.LocalDateTime;
 import java.util.List;
 
+
 @RestController
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class AnimeController {
+
+    @Autowired
+    private DateUtil dateUtil;
+
+    Logger logger = LoggerFactory.getLogger(AnimeController.class);
 
     @GetMapping(path = "lista_animes")
     public List<Anime> list(){
+        logger.info(dateUtil.formatLocalDatetimeToDatabaseStyle(LocalDateTime.now()));
         return List.of(new Anime("Saint Seya"), new Anime("Shurato"));
     }
 
